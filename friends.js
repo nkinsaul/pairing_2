203efc84c77c8e34a20f83e8984f83f57af8f998
Natalie Kinsaul <n.kinsaul@gmail.com>
2022-09-21 16:30:20 -0700

    Add veryBestFriend

--- friends.js
+++ friends.js
@@ -1,6 +1,9 @@
 var bestFriend = "Nim";
+var veryBestFriend = "Bob";
 if (bestFriend === "Nim") {
   console.log("Please eat something");
+} else if (veryBestFriend === "Bob"){
+  console.log("Why are you floating?");
 } else if (bestFriend !== "Nim"){
   console.log("Who are you?");
 }
